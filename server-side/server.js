5b8cb9d433dfc99f5d7ca10ae1313eb9788c8de1
Harsh Kurware <harshkurware03@gmail.com>
2025-08-22 20:39:08 +0530

    we are so back now

--- server-side/server.js
+++ server-side/server.js
@@ -1,1 +1,3 @@
 //this is demo to guess re start is working please confirm this from github
+
+//second demo coz i fucked up again by pissed of VS code after fucked up pretty bad i got api key and all back and gotta go
